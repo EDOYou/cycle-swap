085b6afa766de2708cbe3b577cbc7f89b612528c
Taghiyev <ktaghiyev01@gmail.com>
2022-12-02 03:03:40 +0100

    cycle-swap

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,42 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 class CycleSwap {
+
     static void cycleSwap(int[] array) {
 
+        int[] auxArr = new int[array.length];
+
+        if (array.length != 0) {
+            auxArr[0] = array[auxArr.length - 1];
+            int cnt = 1;
+            for (int i = 0; i < array.length - 1; i++) {
+                auxArr[cnt] = array[i];
+                cnt++;
+            }
+        }
+        System.arraycopy(auxArr,0,array,0,auxArr.length);
+    }
+
+    static int[] cycleSwap2(int[] array) {
+
+        int[] auxArr = new int[array.length];
+        auxArr[0] = array[auxArr.length - 1];
+        int cnt = 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            auxArr[cnt] = array[i];
+            cnt++;
+        }
+        System.arraycopy(auxArr,0,array,0,auxArr.length);
+        return array;
     }
 
     static void cycleSwap(int[] array, int shift) {
 
+        if (array.length != 0) {
+            if (shift != 0)
+                cycleSwap(cycleSwap2(array),shift - 1);
+        }
     }
 }
